864b399a5ae64a24fbc2cdd0ff1476345db2fea9
Yesica Ramirez <yesmagaly@gmail.com>
2018-01-18 11:33:46 -0500

    Added slick configurations.

--- web/themes/custom/beauty/js/carousel.js
+++ web/themes/custom/beauty/js/carousel.js
@@ -1,1 +1,14 @@
-console.log('hi');
+(function demo ($, Drupal) {
+  Drupal.behaviors.carousel = {
+    attach: function (context, settings) {
+      $(".slick-carousel__content").slick({
+        speed: 500,
+        slidesToShow: 3,
+        centerMode: true,
+        infinite: true,
+        variableWidth: true,
+        arrows: false
+      });
+    }
+  }
+})(jQuery, Drupal)
